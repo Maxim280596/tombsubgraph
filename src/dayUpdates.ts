ec956bcb20687f145f8d0b85285d14e2591c0d57
Maxim280596 <max280596@gmail.com>
2022-03-30 09:37:38 +0300

    add apy

---
+++ src/dayUpdates.ts
@@ -0,0 +1,125 @@
+// import { PairHourData } from './../types/schema'
+/* eslint-disable prefer-const */
+import { BigInt, BigDecimal} from '@graphprotocol/graph-ts'
+import { toDecimal } from './helpers';
+import {StratHarvest, TombFees} from "../generated/StrategyCommonChefLPProxy/StrategyCommonChefLPProxy";
+import { StrategyDayData, StrategyData, FeesData } from "../generated/schema";
+
+
+export function pow(base: BigDecimal, exponent: number): BigDecimal {
+  let result = base;
+
+  if (exponent == 0) {
+    return BigDecimal.fromString('1');
+  }
+
+  for (let i = 2; i <= exponent; i++) {
+    result = result.times(base);
+  }
+
+  return result;
+}
+
+
+export function updateStrategyDayData(event: StratHarvest): StrategyDayData {
+  let timestamp = event.block.timestamp.toI32()
+  let dayID = timestamp / 86400
+  let dayStartTimestamp = dayID * 86400
+  let strategyDayData = StrategyDayData.load(dayID.toString())
+  if (strategyDayData === null) {
+    strategyDayData = new StrategyDayData(dayID.toString())
+    strategyDayData.date = dayStartTimestamp
+    strategyDayData.earned = BigDecimal.fromString('0');
+    strategyDayData.tvl = BigDecimal.fromString('0');
+    strategyDayData.harvestsCount = BigInt.fromI32(0);
+    strategyDayData.startDayTvl = BigDecimal.fromString('0');
+    strategyDayData.dailyAPR = BigDecimal.fromString('0');
+    strategyDayData.yearlyAPY = BigDecimal.fromString('0');
+    strategyDayData.yearlyCompaundAPY = BigDecimal.fromString('0');
+    strategyDayData.dailyAPRRaw = BigInt.fromI32(0);
+    strategyDayData.earnedRaw = BigInt.fromI32(0);
+    strategyDayData.tvlRaw = BigInt.fromI32(0);
+  }
+  if(strategyDayData.startDayTvl == BigDecimal.fromString('0')) {
+    strategyDayData.startDayTvl = toDecimal(event.params.tvl);
+  }
+  strategyDayData.earned = strategyDayData.earned.plus(toDecimal(event.params.wantHarvested));
+  strategyDayData.tvl = toDecimal(event.params.tvl);
+  strategyDayData.earnedRaw = strategyDayData.earnedRaw.plus(event.params.wantHarvested);
+  strategyDayData.tvlRaw = event.params.tvl;
+  strategyDayData.dailyAPRRaw = strategyDayData.earnedRaw.times(BigInt.fromI32(100)).div(strategyDayData.tvlRaw);
+  strategyDayData.harvestsCount = strategyDayData.harvestsCount.plus(BigInt.fromI32(1));
+  strategyDayData.dailyAPR = strategyDayData.earned.times(BigDecimal.fromString('100')).div(strategyDayData.tvl)
+  strategyDayData.yearlyAPY = strategyDayData.dailyAPR.times(BigDecimal.fromString('365'))
+  // let yearlyHarvestCount = strategyDayData.harvestsCount.times(BigInt.fromI32(365))
+  // let numberCount = toDecimal(yearlyHarvestCount)
+
+ 
+  let divYearlyApy = strategyDayData.yearlyAPY.div(BigDecimal.fromString('100'));
+  let indexForAutoComaundAPY = divYearlyApy.div(BigDecimal.fromString('1460'))
+  indexForAutoComaundAPY = indexForAutoComaundAPY.plus(BigDecimal.fromString('1'))
+  let powAPR = pow(indexForAutoComaundAPY, 1460)
+  powAPR = powAPR.minus(BigDecimal.fromString('1'))
+  powAPR = powAPR.times(BigDecimal.fromString('100'))
+  // let step1 = strategyDayData.dailyAPRRaw.div(BigInt.fromI32(100))
+  // let step2 = step1.div(BigInt.fromI32(1460))
+
+ 
+
+  // let percent = pow(resultDailyAPR, 1460)
+  // let res = percentRaw.div(numberCount)
+  // for (let i = 2; i <= 1460; i++) {
+  //   res = res.times(base);
+  // }
+  strategyDayData.yearlyCompaundAPY = powAPR
+  // strategyDayData.yearlyCompaundAPY = BigDecimal.fromString('1').plus(strategyDayData.dailyAPR.div(toDecimal(strategyDayData.harvestsCount).times(BigDecimal.fromString('365'))))
+  // strategyDayData.earnedPercent = 0;
+  strategyDayData.save()
+
+  return strategyDayData as StrategyDayData
+}
+
+export function updateStrategyData(event: StratHarvest): StrategyData {
+  // let timestamp = event.block.timestamp.toI32()
+  // let dayID = timestamp / 86400
+  // let dayStartTimestamp = dayID * 86400
+  let strategyData = StrategyData.load(event.address.toHexString())
+  if (strategyData === null) {
+    strategyData = new StrategyData(event.address.toHexString())
+    strategyData.totalEarned = BigDecimal.fromString('0');
+    strategyData.tvl = BigDecimal.fromString('0');
+    strategyData.totalHarvestsCount = BigInt.fromI32(0);
+  }
+
+  strategyData.totalEarned = strategyData.totalEarned.plus(toDecimal(event.params.wantHarvested));
+  strategyData.tvl = toDecimal(event.params.tvl);
+  strategyData.totalHarvestsCount = strategyData.totalHarvestsCount.plus(BigInt.fromI32(1));
+  // strategyDayData.earnedPercent = 0;
+  strategyData.save()
+
+  return strategyData as StrategyData
+}
+
+export function updateFeesData(event: TombFees): FeesData {
+  // let timestamp = event.block.timestamp.toI32()
+  // let dayID = timestamp / 86400
+  // let dayStartTimestamp = dayID * 86400
+  let feesData = FeesData.load(event.address.toHexString().concat('-').concat('fees'))
+  if (feesData === null) {
+    feesData = new FeesData(event.address.toHexString().concat('-').concat('fees'))
+    feesData.totalFees = BigDecimal.fromString('0');
+    feesData.lastPaidFeesAmount = BigDecimal.fromString('0');
+    feesData.lastPaidFeesDate = BigInt.fromI32(0);
+    feesData.feesCount = BigInt.fromI32(0);
+  }
+
+  // feesData.totalFees = feesData.totalFees.plus(toDecimal(event.params.feesAmount));
+  // feesData.lastPaidFeesAmount = toDecimal(event.params.feesAmount);
+  // feesData.lastPaidFeesDate = event.block.timestamp;
+  // feesData.feesCount = feesData.feesCount.plus(BigInt.fromI32(1));
+  // strategyDayData.earnedPercent = 0;
+  feesData.save()
+
+  return feesData as FeesData
+}
+
--- src/mapping.ts
+++ src/mapping.ts
@@ -10,6 +10,7 @@ import {
   Withdraw
 } from "../generated/StrategyCommonChefLPProxy/StrategyCommonChefLPProxy"
 import { ExampleEntity } from "../generated/schema"
+import { updateStrategyDayData, updateStrategyData, updateFeesData } from './dayUpdates';
 
 export function handleDeposit(event: Deposit): void {
   // Entities can be loaded from the store using a string ID; this ID
@@ -88,9 +89,35 @@ export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
 
 export function handlePaused(event: Paused): void {}
 
-export function handleStratHarvest(event: StratHarvest): void {}
+export function handleStratHarvest(event: StratHarvest): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = ExampleEntity.load(event.transaction.from.toHex())
 
-export function handleTombFees(event: TombFees): void {}
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new ExampleEntity(event.transaction.from.toHex())
+
+    // Entity fields can be set using simple assignments
+    entity.count = BigInt.fromI32(0)
+  }
+
+  // BigInt and BigDecimal math are supported
+  entity.count = entity.count + BigInt.fromI32(1)
+
+  // Entity fields can be set based on event parameters
+  entity.tvl = event.params.tvl
+  updateStrategyData(event);
+  updateStrategyDayData(event);
+
+  // Entities can be written to the store with `.save()`
+  entity.save()
+}
+
+export function handleTombFees(event: TombFees): void {
+  updateFeesData(event)
+}
 
 export function handleUnpaused(event: Unpaused): void {}
 
